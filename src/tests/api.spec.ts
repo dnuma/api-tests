6d29cd7a5c47f2d4e1ae300c0762069a87040809
David Numa <david.numa@gmail.com>
2025-11-12 22:41:44 -0500

    User Interface

--- src/tests/api.spec.ts
+++ src/tests/api.spec.ts
@@ -2,6 +2,7 @@ import { log } from 'console';
 import { test, expect } from 'playwright/test';
 import { ApiCalls } from '../utils/apiCalls.utils';
 import { faker } from '@faker-js/faker';
+import { User } from '../interfaces/user.interface';
 
 test.describe('ExpandTesting.com API tests', {
   tag: ['@api', '@ExpandTesting'],
@@ -17,9 +18,14 @@ test.describe('ExpandTesting.com API tests', {
     log(`Starting test: ${testinfo.title}`);
     log(`Base URL is: ${baseURL}`);    
     const apiCalls = new ApiCalls();
-    const fullName = faker.person.fullName();
-    const email = faker.internet.email();
-    const password = faker.internet.password( { length: 10 });
+    let demoUserData: User = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password( { length: 10 }),
+      phone: '',
+      company: ''
+    };
+    let token: string;
 
     await test.step('Health Check', async () => {
       log('Performing health check...');  
@@ -29,28 +35,37 @@ test.describe('ExpandTesting.com API tests', {
     });
 
     await test.step('Register an user', async () => {
-      log('Creating a new user: ' + fullName + ', ' + email);  
+      log('Creating a new user: ' + demoUserData.name + ', ' + demoUserData.email);  
       const response = await apiCalls.registerUser( 
         request, 
         baseURL!,
-        fullName,
-        email,
-        password
+        demoUserData.name,
+        demoUserData.email,
+        demoUserData.password
       );
-      expect(response.status).toBe(201);
+      expect(await response.status).toBe(201);
       log('User registration passed with status 201.');
     });
 
     await test.step('Login', async () => {
-      log('Logging in with: ' + email + ', password: ' + password);  
+      log('Logging in with: ' + demoUserData.email + ', password: ' + demoUserData.password);  
       const response = await apiCalls.login( 
         request, 
         baseURL!,
-        email,
-        password
+        demoUserData.email,
+        demoUserData.password
       );
-      expect(response.status).toBe(200);
+      expect(await response.status).toBe(200);
       log('User login passed with status 200.');
+      token = await response.data.token;
+    });
+
+    await test.step('Update profile', async () => {
+      log('Updating profile for user: ' + demoUserData.name);  
+      log(`New name: ${demoUserData.name = faker.person.fullName()}`);
+      log(`New phone: ${demoUserData.phone = faker.phone.number({ style: 'international' })}`);
+      log(`New company: ${demoUserData.company = faker.company.name()}`);
+      
     });
 
 
